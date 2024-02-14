bb931d75dd159f85c134320965567cfc83ad947d
evanleonille <133546962+evanleonille@users.noreply.github.com>
2024-02-14 16:03:13 +0800

    fixed charts for temporarry

--- js/health-chart.js
+++ js/health-chart.js
@@ -1,35 +1,45 @@
 document.addEventListener('DOMContentLoaded', function () {
-  var ctx = document.getElementById('healthChart').getContext('2d');
-  var myChart = new Chart(ctx, {
-    type: 'radar',
-    data: {
-      labels: ['Blood Pressure', 'Heart Rate', 'BMI',],
-      datasets: [{
-        label: 'Orphan Health Metrics',
-        data: [120, 80, 25], // Example values
-        backgroundColor: 'rgba(75, 192, 192, 0.2)',
-        borderColor: 'rgba(75, 192, 192, 1)',
-        borderWidth: 2
-      }]
-    },
-    options: {
-      responsive: true,
-      plugins: {
-        legend: {
-          position: 'top',
-        },
-        title: {
-          display: true,
-          text: 'Orphan Health Metrics'
-        }
+    var ctx = document.getElementById('overallChart').getContext('2d');
+    var myChart = new Chart(ctx, {
+      type: 'horizontalBar',
+      data: {
+        labels: ['Number of Orphans', 'Average Age', 'Average BMI', 'Average Health Score'],
+        datasets: [{
+          label: 'Orphanage Overview',
+          data: [100, 12, 23, 75], // Example values
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.5)',
+            'rgba(54, 162, 235, 0.5)',
+            'rgba(255, 206, 86, 0.5)',
+            'rgba(75, 192, 192, 0.5)'
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)'
+          ],
+          borderWidth: 1
+        }]
       },
-      scale: {
-        ticks: {
-          beginAtZero: true,
-          min: 0,
-          max: 250 // Example maximum value
+      options: {
+        responsive: true,
+        plugins: {
+          legend: {
+            display: false,
+          },
+          title: {
+            display: true,
+            text: 'Orphanage Overall Details'
+          }
+        },
+        scales: {
+          x: {
+            ticks: {
+              beginAtZero: true
+            }
+          }
         }
       }
-    }
+    });
   });
-});
