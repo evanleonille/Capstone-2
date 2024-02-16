af29a57ef6e45dc0779b283075acc416b0728f8a
evanleonille <133546962+evanleonille@users.noreply.github.com>
2024-02-16 16:29:57 +0800

    added profile and trash

---
+++ js/trash.js
@@ -0,0 +1,10 @@
+        // JavaScript code for handling delete and empty trash buttons
+        document.getElementById('deleteSelectedFiles').addEventListener('click', function() {
+            // Logic to delete selected files
+            // You can retrieve selected files using their checkboxes and perform delete action
+        });
+
+        document.getElementById('emptyTrash').addEventListener('click', function() {
+            // Logic to empty the trash
+            // You can delete all files in the trash
+        });
